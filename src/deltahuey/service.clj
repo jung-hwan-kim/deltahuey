123cb8877b813fb66b7dad44642136253995093a
Jung Kim <jukim@kinaxis.com>
2021-05-28 23:18:44 -0400

    :art: rest

--- src/deltahuey/service.clj
+++ src/deltahuey/service.clj
@@ -4,47 +4,61 @@
             [io.pedestal.http.route :as route]
             [io.pedestal.http.body-params :as body-params]
             [clojure.tools.logging :as log]
+            [cheshire.core :as json]
             [io.pedestal.http.route.definition :refer [defroutes]])
   (:import (org.apache.spark.sql SparkSession)
            (com.gnosyz.deltahuey SparkApp)))
 
 (def spark (atom nil))
 
+(defn return-json[body-map]
+  {:status 200
+    :headers {"Content-Type" "application/json"}
+    :body (json/generate-string body-map)
+    })
+
 (defn status
   [request]
   (let [name (get-in request [:params :name] "World")]
-    {:status 200 :body {:status "ok" :spark @spark}}))
+    (return-json {:spark spark})))
 
 (defn start-spark [request]
-  (when (nil? @spark)
-    (println "### ---> starting")
-    (reset! spark (new SparkApp "ingest-3"))
-    (.start spark))
-  {:status 200 :body {:status "spark-running"}})
+  (if (nil? @spark)
+    (do
+      (println "### ---> starting")
+      (reset! spark (new SparkApp "ingest-3"))
+      (.start spark)
+      (return-json {:status "Spark started."}))
+    (do
+      (println "### already running")
+      (return-json {:status "Spark already running."}))))
 
 (defn stop-spark [request]
   (if (nil? @spark)
-    (log/info "nothing to do.. it's already stopped")
+    (do
+      (log/info "nothing to do.. it's already stopped")
+      (return-json {:result "Nothing to stop."}))
     (try
       (println "stopping..")
       (.stop @spark)
-      (reset! spark nil)))
-    {:status 200 :body {:cmd "stopped"}}
-  )
+      (reset! spark nil)
+      (return-json {:result "Stopped"}))))
 
 (defn ingest [request]
-  (let [message (get-in request [:params :message] "ingest-33")]
-    (log/info "ingesting:" message)
+  (let [body (json/parse-stream (clojure.java.io/reader (:body request)) true)
+        path (:path body)
+        message (:message body)
+        ]
+    (log/info "ingesting:" path "," message)
     (.log @spark message)
-    {:status 200 :body "ingested"}))
-
+    (return-json {:path path :message message :status "Success"})))
 
 (defroutes routes
            [[["/"
               ["/status" {:get status}]
               ["/start" {:get start-spark}]
               ["/stop" {:get stop-spark}]
-              ["/ingest" {:get ingest}]
+              ["/ingest" {:post ingest}]
               ]]])
 
 (def service {:env                 :prod
