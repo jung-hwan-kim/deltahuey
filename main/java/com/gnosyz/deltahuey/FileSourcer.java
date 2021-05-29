7c4de64cdbb216fda21000d1612fc1e2340f7dd1
Jung Kim <jukim@kinaxis.com>
2021-05-29 17:51:11 -0400

    :constructions: fixed archiving

--- main/java/com/gnosyz/deltahuey/FileSourcer.java
+++ main/java/com/gnosyz/deltahuey/FileSourcer.java
@@ -4,17 +4,28 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.streaming.StreamingQuery;
+import org.apache.spark.sql.types.*;
 
 public class FileSourcer {
+    private static StructType treeSchema = new StructType(new StructField[] {
+            new StructField("id", DataTypes.StringType, false, Metadata.empty()),
+            new StructField("girth", DataTypes.DoubleType, true, Metadata.empty()),
+            new StructField("height", DataTypes.DoubleType, true, Metadata.empty()),
+            new StructField("volume", DataTypes.DoubleType, true, Metadata.empty())
+    });
+
     public static void main(String[] args) {
 
         // Listening to file
         SparkSession spark = SparkSession.builder().appName("File Sourcer").getOrCreate();
         spark.log().info("###-###-<START>-###-###");
 
-        Dataset<Row> rows = spark.readStream().format("file")
+
+        Dataset<Row> rows = spark.readStream()
                 .option("cleanSource", "archive")
-                .option("sourceArchiveDir", "/var/data/file-archive")
+                .option("sourceArchiveDir", "/var/data/archive")
+                .schema(treeSchema)
+                .format("csv")
                 .load("/var/data/file-source");
         try {
             StreamingQuery query = rows.writeStream().format("console").start();
